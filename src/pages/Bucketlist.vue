c59e9a41dbcb5786c2f2613938b91359d0279aa1
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2020-03-18 22:32:51 +0900

    update add bucketlist

--- src/pages/Bucketlist.vue
+++ src/pages/Bucketlist.vue
@@ -7,6 +7,8 @@
         <button v-on:click="deleteBucketlist(bucketlist)">del</button>
       </li>
     </ul>
+    <input class="input" type="title" v-model="title" name="title">
+    <input class="input" type="text" v-model="text" name="text">
     <button v-on:click="getBucketlist">げっと</button>
     <button v-on:click="saveBucketlist">せーぶ</button>
   </div>
@@ -41,15 +43,18 @@ export default {
       })
     },
     saveBucketlist(){
+      var _this = this;
       var token = localStorage.getItem('access_token')
       var user_id = localStorage.getItem('user_id')
       let data = {
-          title:"title",
-          text:"text",
+          title:this.title,
+          text:this.text,
           user_id: localStorage.getItem('user_id')
       }
       axios.post('/api/v1/users/'+ user_id +'/bucketlists',data,{headers:{'Authorization': 'JWT ' + token}}).then(function(response){
         console.log(response.data);
+        _this.title = '';
+        _this.text = '';
       })
     },
     deleteBucketlist(bucketlist){
