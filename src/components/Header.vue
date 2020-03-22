42d8f2c2ec8fa28d19d0d0ab1bf1e03a67c4c264
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2020-03-22 14:38:07 +0900

    ヘッダー調整

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="header">
-    <router-link to="/"> home </router-link>
-    <router-link to="/profile"> profile </router-link>
-    <router-link to="/bucketlist"> bucketlist </router-link>
-    <router-link to="/signin"> signin </router-link>
-    <router-link to="/signup"> signup </router-link>
+    <v-toolbar>
+      <v-toolbar-title class="display-2 font-weight-black">Bucketlist</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn>
+          <router-link to="/"> home </router-link>
+        </v-btn>
+        <v-btn>
+          <router-link to="/profile"> profile </router-link>
+        </v-btn>
+        <v-btn>
+          <router-link to="/bucketlist"> bucketlist </router-link>
+        </v-btn>
+        <v-btn>
+          <router-link to="/signin"> signin </router-link>
+        </v-btn>
+        <v-btn>
+          <router-link to="/signup"> signup </router-link>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
   </div>
 </template>
