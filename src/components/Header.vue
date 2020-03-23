25db7f2783659f42fc7a817d5f0ee1f4fbfa6038
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2020-03-23 22:45:26 +0900

    ドロップダウン対応

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,48 @@
 <template>
-  <v-card color="grey lighten-4" flat height="70px" tile>
-    <v-toolbar>
-      <v-toolbar-title class="display-2 font-weight-black"
-        >Bucketlist</v-toolbar-title
-      >
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn to="/">
-          home
-        </v-btn>
-        <v-btn to="/profile">
-          profile
-        </v-btn>
-        <v-btn to="/bucketlist">
-          bucketlist
-        </v-btn>
-        <v-btn to="/signin">
-          signin
-        </v-btn>
-        <v-btn to="/signup">
-          signup
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-  </v-card>
+  <v-app-bar>
+    <v-toolbar-title class="display-2 font-weight-black"
+      >Bucketlist</v-toolbar-title
+    >
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn to="/">
+        home
+      </v-btn>
+      <v-btn to="/bucketlist">
+        bucketlist
+      </v-btn>
+      <v-btn to="/signin">
+        signin
+      </v-btn>
+      <v-btn to="/signup">
+        signup
+      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn icon large v-on="on">
+            <v-avatar size="32px" item>
+              <v-img
+                src="../assets/logo.png"
+            /></v-avatar>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item>
+            <v-list-item-title
+              ><router-link to="/profile">
+                profile
+              </router-link></v-list-item-title
+            >
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title
+              ><router-link to="/logout">
+                logout
+              </router-link></v-list-item-title
+            >
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar-items>
+  </v-app-bar>
 </template>
