52ad1224b5d2b2896f76280ed7c778bbdce71aaa
yoshitaku-jp <yoshimura.takuya1990@gmail.com>
2020-03-28 21:15:46 +0900

    update vuex-persistedstate

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 Vue.use(Vuex);
 
 import axios from "axios";
@@ -7,7 +8,8 @@ import jwt_decode from "jwt-decode";
 
 export default new Vuex.Store({
     state: {
-        userId : ""
+        userId : "",
+        token : "",
     },
     mutations:{
         setUserId(state,newValue){
@@ -15,23 +17,32 @@ export default new Vuex.Store({
         },
         clearUserId(state){
             state.userId = ""
-        }
+        },
+        setToken(state,newValue){
+            state.token = newValue
+        },
+        clearToken(state){
+            state.token = ""
+        },        
     },
     getters: {
         userId: function(state)  {
             return state.userId
+        },
+        token: function(state)  {
+            return state.token
         }
     },
     actions: {
-        setUserId: function(context,payload){
+        setUser: function(context,payload){
 
             axios.post("/api/v1/token", payload).then(function (response) {
                 let token = response.data["token"];
-        
+                context.commit("setToken", token); 
                 const decoded = jwt_decode(token);
                 context.commit("setUserId", decoded.user_id);
               });
             },
-        }
-    
+        },
+        plugins: [createPersistedState()]
 })
